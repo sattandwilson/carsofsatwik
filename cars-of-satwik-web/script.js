133afd8cdafc2e49e4987047a6a2ed4e555783c0
satwik <wilson54t333@gamil.com>
2024-02-16 12:32:33 +0530

    Add initial files

---
+++ cars-of-satwik-web/script.js
@@ -0,0 +1,37 @@
+// function about(){
+    
+// }
+
+
+
+
+function ferrariPista() {
+    window.location.href="pista.html";
+}
+function saleen() {
+    window.location.href="saleen.html";
+}
+function huracan() {
+    window.location.href="huracan.html";
+}
+function ferrariEnzo() {
+    window.location.href="enzo.html";
+}
+function koenigsegg() {
+    window.location.href="jesko.html";
+}
+function hennessey() {
+    window.location.href="venom.html";
+}
+function porsche() {
+    window.location.href="gt3.html";
+}
+function lykan() {
+    window.location.href="lykan.html";
+}
+function pagani() {
+    window.location.href="huayra.html";
+}
+function bmw() {
+    window.location.href="bmw.html";
+}
